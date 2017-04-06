9cbb7b4412c33e82a1f64d3eb056919f8092bb2b
Zach Lipps <zacharylipps10@gmail.com>
2017-04-06 16:39:49 -0700

    add start game feature which adds players in lobby to player array

--- src/actions/game.js
+++ src/actions/game.js
@@ -2,16 +2,81 @@ import { database } from '../firebase';
 import keys from 'lodash/keys';
 import market from '../Cards/cards';
 
-const startGameAction = user => ({
+const startGameAction = playerArr => ({
   type: 'START_GAME',
-  user,
+  playerArr,
 });
 
+const initializePlayer = (uid, idx) => database.ref(`/users/${uid}`).once('value', snapshot => snapshot)
+  .then((user) => {
+    const playerObj = Object.assign({}, {
+      uid,
+      turnOrder: idx,
+      displayName: user.val().displayName,
+      stats: {
+        energy: 0,
+        health: 10,
+        points: 0,
+      },
+    });
+    return playerObj;
+  });
+
+
 export const startGame = () => (dispatch) => {
-  database.ref('/users').once('value', snapshot => (snapshot.val()))
-    .then(ukeys => ukeys.val()[keys(ukeys.val())[0]])
-    .then((userID) => { database.ref('/currentPlayer').set({ displayName: userID.displayName, uid: userID.uid }); return userID.displayName; })
-    .then(user => dispatch(startGameAction(user)))
-    .then(() => database.ref('market/').set(market));
+  database.ref('/PlayersInGame').once('value', snapshot => (snapshot.val()))
+    .then((userIDS) => {
+      if (userIDS.val()) {
+        userIDS.val().forEach((userID, idx) => {
+          initializePlayer(userID, idx)
+          .then((playerObj) => {
+            const playerArr = [];
+            playerArr.push(playerObj);
+            return playerArr;
+          })
+          .then((playerArr) => {
+            // eventually this will set at gameID/playerArr
+            database.ref('playerArr').set(playerArr);
+          })
+          .then(() => database.ref('playerArr').once('value', (snapshot) => {
+            dispatch(startGameAction(snapshot.val()));
+          }));
+        });
+      }
+    });
+
+
+   // .then(() => database.ref('market/').set(market));
 };
 
+
+// initialize the stats
+
+    // .then((userID) => {
+    //   const user = userID.val();
+    //   database.ref('/currentPlayer').set({ displayName: user.displayName, uid: user.uid }); return user.displayName;
+    // })
+
+    // .then(user => dispatch(startGameAction(user)))
+
+
+// export const startListeningToAuthChanges = () => (dispatch) => {
+//   auth.onAuthStateChanged((user) => {
+//     if (user) {
+//       dispatch(signedIn(user));
+
+//       const obj = Object.assign({}, pick(user, ['displayName', 'photoURL', 'email', 'uid']), {
+//         currentlyOn: true,
+//         stats: {
+//           energy: 0,
+//           health: 10,
+//           points: 0,
+//         },
+//       });
+
+//       usersRef.child(user.uid).set(obj);
+//     } else {
+//       dispatch(signedOut());
+//     }
+//   });
+// };
