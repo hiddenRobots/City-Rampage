6011f95b452658aeba90153705ef2920f3d5a9ce
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-06 23:56:12 -0700

    Do things the right way by adding the firebase file which you definitely be commited

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+import config from '../config.js';
+
+console.log(config);
+
+firebase.initializeApp(config);
+
+export default firebase;
+export const database = firebase.database();
+
+const marketRef = database.ref('/market');
+
+export const addCardToHand = (buyerHash, card) => database.ref(`/users/${buyerHash}/hand`).push(card);
+
+export const updateMarket = copy => marketRef.set(copy);
+
+export const getMarket = _ => new Promise((resolve, reject) => {
+  marketRef.once('value', (snapshot) => {
+    const copy = snapshot.val();
+    // No error handling :(
+    resolve(copy);
+  });
+});
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
