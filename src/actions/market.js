4863b03062e455473791800bb65b376c3efe5a9b
Jaime Pericas <jaime.pericas.saez@gmail.com>
2017-04-06 23:37:56 -0700

    Switching to Firebase functions

--- src/actions/market.js
+++ src/actions/market.js
@@ -1,12 +1,10 @@
 import shuffle from 'lodash/shuffle';
-import { database } from '../firebase';
-const marketRef = database.ref('/market');
-
+import { addCardToHand, updateMarket, getMarket } from '../firebase';
 
 function firebaseFix(obj) {
-  if (!obj.deck) obj.deck = [];
-  if (!obj.face_up) obj.face_up = [];
-  if (!obj.discarded) obj.discarded = [];
+  obj.deck = obj.deck || [];
+  obj.face_up = obj.face_up || [];
+  obj.discarded = obj.discarded || [];
 }
 
 function regenDeckIfEmpty(obj) {
@@ -24,32 +22,32 @@ function dealCard(obj) {
 
 // add logic that checks whether the player can buy; and substract the energy
 export const buyCard = (card, buyer) => (dispatch) => {
-  marketRef.once('value', (snapshot) => {
-    const copy = snapshot.val();
-    firebaseFix(copy);
-    copy.face_up = copy.face_up.filter(c => c.title !== card.title);
-    if (copy.deck.length > 0) {
-      dealCard(copy);
-      regenDeckIfEmpty(copy);
-    }
-    marketRef.set(copy)
-    .then(() => database.ref(`/users/${buyer}/hand`).push(card))
-    .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
-  });
+  getMarket()
+    .then((copy) => {
+      firebaseFix(copy);
+      copy.face_up = copy.face_up.filter(c => c.title !== card.title);
+      if (copy.deck.length > 0) {
+        dealCard(copy);
+        regenDeckIfEmpty(copy);
+      }
+      return updateMarket(copy)
+        .then(() => addCardToHand(buyer, card))
+        .then(() => dispatch({ type: 'DEAL_CARD', payload: copy }));
+    });
 };
 
 export const resetMarket = () => (dispatch) => {
-  marketRef.once('value', (snapshot) => {
-    const copy = snapshot.val();
-    firebaseFix(copy);
-    regenDeckIfEmpty(copy);
-    copy.face_up.forEach(card => copy.discarded.push(card));
-    copy.face_up = [];
-    for (let i = 0; i < 3; i++) {
-      dealCard(copy);
+  getMarket()
+    .then((copy) => {
+      firebaseFix(copy);
       regenDeckIfEmpty(copy);
-    }
-    marketRef.set(copy)
-    .then(() => dispatch({ type: 'DEAL_NEW_MARKET', payload: copy }));
-  });
+      copy.face_up.forEach(card => copy.discarded.push(card));
+      copy.face_up = [];
+      for (let i = 0; i < 3; i++) {
+        dealCard(copy);
+        regenDeckIfEmpty(copy);
+      }
+      return updateMarket(copy)
+        .then(() => dispatch({ type: 'DEAL_NEW_MARKET', payload: copy }));
+    });
 };
